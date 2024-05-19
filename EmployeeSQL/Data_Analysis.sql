32141e07ab13d264a1f33fa69749b7acacb58c67
Rock2638 <162598415+Rock2638@users.noreply.github.com>
2024-05-19 18:11:29 +0800

    sql_challenge nearly finished

--- EmployeeSQL/Data_Analysis.sql
+++ EmployeeSQL/Data_Analysis.sql
@@ -101,10 +101,7 @@ OR
 --Q8 - Listing the frequency counts, in descending order, of all the employee last names 
 -- (that is, how many employees share each last name).
 SELECT last_name, COUNT(last_name) AS "total count"
-
 FROM employees
-
 GROUP BY last_name
-
 ORDER BY "total count" DESC;
 
