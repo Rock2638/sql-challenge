62af30d22fdc06832ac4b215fd754f99a5653b73
Rock2638 <162598415+Rock2638@users.noreply.github.com>
2024-05-19 15:24:27 +0800

    sql_challenge nearly finished

---
+++ EmployeeSQL/Data_Analysis.sql
@@ -0,0 +1,110 @@
+-- Q1 - Listing the employee number, last name, first name, sex, and salary of each employee.
+SELECT 
+    employees.emp_no,
+    employees.last_name,
+    employees.first_name,
+    employees.sex,
+    salaries.salary
+FROM 
+    employees
+INNER JOIN salaries ON employees.emp_no = salaries.emp_no;
+
+
+-- Q2 - Listing the first name, last name, and hire date for the employees who were hired in 1986.
+SELECT 
+    first_name,
+    last_name,
+    hire_date
+FROM 
+    employees
+WHERE 
+    EXTRACT(YEAR FROM hire_date) = 1986;
+
+
+-- Q3 -  Listing the manager of each department along with their department number, 
+-- department name, employee number, last name, and first name.
+SELECT 
+   	departments.dept_name,
+    dept_manager.dept_no,
+    employees.emp_no,
+    employees.last_name,
+    employees.first_name
+FROM 
+    departments 
+INNER JOIN dept_manager ON departments.dept_no = dept_manager.dept_no
+INNER JOIN employees ON employees.emp_no = dept_manager.emp_no
+ORDER BY departments.dept_name
+
+
+-- Q4 - Listing the department number for each employee along with that employee’s employee number, 
+-- last name, first name, and department name.
+SELECT 
+   	departments.dept_name,
+    dept_emp.dept_no,
+    employees.emp_no,
+    employees.last_name,
+    employees.first_name
+FROM 
+    departments 
+INNER JOIN dept_emp ON departments.dept_no = dept_emp.dept_no
+INNER JOIN employees ON employees.emp_no = dept_emp.emp_no
+ORDER BY departments.dept_name, employees.last_name
+
+
+-- Q5 - List the first name, last name, and sex of each employee whose first name is 
+-- Hercules and whose last name begins with the letter B.
+SELECT 
+	first_name,
+    last_name,
+	sex
+FROM 
+	employees
+WHERE 
+	first_name='Hercules'
+AND
+	last_name LIKE 'B%';
+
+
+-- Q6 - Listing each employee in the Sales department, including their employee number, last name, and first name.
+SELECT 
+   	departments.dept_name,
+    dept_emp.emp_no,
+    employees.last_name,
+    employees.first_name
+FROM 
+    departments 
+INNER JOIN dept_emp ON departments.dept_no = dept_emp.dept_no
+INNER JOIN employees ON employees.emp_no = dept_emp.emp_no
+
+WHERE 
+	departments.dept_name = 'Sales';
+
+
+-- Q7 - Listing each employee in the Sales and Development departments, including their employee number, 
+-- last name, first name, and department name.
+SELECT 
+   	departments.dept_name,
+    dept_emp.emp_no,
+    employees.last_name,
+    employees.first_name
+FROM 
+    departments 
+INNER JOIN dept_emp ON departments.dept_no = dept_emp.dept_no
+INNER JOIN employees ON employees.emp_no = dept_emp.emp_no
+
+WHERE 
+	departments.dept_name = 'Sales' 
+OR 
+	departments.dept_name = 'Development';
+	
+	
+--Q8 - Listing the frequency counts, in descending order, of all the employee last names 
+-- (that is, how many employees share each last name).
+SELECT last_name, COUNT(last_name) AS "total count"
+
+FROM employees
+
+GROUP BY last_name
+
+ORDER BY "total count" DESC;
+
---
+++ EmployeeSQL/Data_Engineering.sql
@@ -0,0 +1,110 @@
+-- Drop table if exists
+DROP TABLE IF EXISTS departments;
+
+-- create a table departments
+CREATE TABLE departments (
+  dept_no VARCHAR PRIMARY KEY,
+  dept_name VARCHAR(30) NOT NULL
+);
+
+SELECT *
+FROM departments
+
+-- Drop table if exists
+DROP TABLE IF EXISTS titles;
+
+-- create a table titles
+CREATE TABLE titles (
+  title_id VARCHAR PRIMARY KEY,
+  title VARCHAR(30) NOT NULL
+);
+
+SELECT * FROM titles
+
+-- Drop table if exists
+DROP TABLE IF EXISTS employees;
+
+-- create a table employees
+CREATE TABLE employees (
+  emp_no INT PRIMARY KEY,
+  emp_title_id VARCHAR(30) NOT NULL,
+  birth_date TEXT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  sex VARCHAR(1) NOT NULL,
+  hire_date TEXT,
+  FOREIGN KEY(emp_title_id) REFERENCES titles(title_id)
+);
+
+SELECT * FROM employees
+
+--Convert birth date text to a new column of type date as import wizard does not convert automatically
+ALTER TABLE employees
+ADD birth_date_date DATE;
+
+UPDATE employees
+SET birth_date_date = TO_DATE(birth_date, 'MM/DD/YYYY')
+WHERE birth_date IS NOT NULL;
+
+ALTER TABLE employees
+DROP birth_date;
+
+ALTER TABLE employees
+RENAME COLUMN birth_date_date TO birth_date;
+
+--Convert hire date text to a new column of type date as import wizard does not convert automatically
+ALTER TABLE employees
+ADD hire_date_date DATE;
+
+UPDATE employees
+SET hire_date_date = TO_DATE(hire_date, 'MM/DD/YYYY')
+WHERE hire_date IS NOT NULL;
+
+ALTER TABLE employees
+DROP hire_date;
+
+ALTER TABLE employees
+RENAME COLUMN hire_date_date TO hire_date;
+
+-- Drop table if exists
+DROP TABLE IF EXISTS salaries;
+
+-- create a table salaries
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
+);
+
+SELECT * FROM salaries
+
+-- Drop table if exists
+DROP TABLE IF EXISTS dept_emp;
+
+-- create a table dept_emp
+CREATE TABLE dept_emp (
+  emp_no INT NOT NULL,
+  dept_no VARCHAR(30)NOT NULL,
+  FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
+  FOREIGN KEY(dept_no) REFERENCES departments(dept_no) 
+);
+
+SELECT * FROM dept_emp
+
+-- Drop table if exists
+DROP TABLE IF EXISTS dept_manager;
+
+-- create a table dept_manager
+CREATE TABLE dept_manager (
+  dept_no VARCHAR(30)NOT NULL,
+  emp_no INT NOT NULL,
+  FOREIGN KEY(dept_no) REFERENCES departments(dept_no), 
+  FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
+);
+
+SELECT * FROM  dept_manager
+
+
+
+
+
